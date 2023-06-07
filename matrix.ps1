a227318770c3daa8e6d87a4e03274c56450c4663
nate <69339422+nanitt@users.noreply.github.com>
2023-06-06 23:40:36 -0400

    Update matrix.ps1

--- matrix.ps1
+++ matrix.ps1
@@ -1,15 +1,12 @@
-function speak {
+# Sets Volume to max level
 
-[CmdletBinding()]
-param (	
-[Parameter (Position=0,Mandatory = $True,ValueFromPipeline=$true)]
-[string]$Sentence
-) 
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
 
-$s.Voice = $s.GetVoices().Item(0)
+# Sets up speech module 
+
 $s=New-Object -ComObject SAPI.SpVoice
 $s.Rate = -2
-$s.Speak($Sentence)
-}
-
-speak "wake up neo".
+$s.Speak("We found you $FN")
+$s.Speak("We know where you are")
+$s.Speak("We are everywhere")
+$s.Speak("Expect us")
